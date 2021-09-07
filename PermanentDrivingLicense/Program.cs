f4edf57976e0f7a0e3c022a47080a89a6b876772
Opilane TTHK <opilane@tthk.ee>
2021-09-07 11:41:28 +0300

    if statements

---
+++ PermanentDrivingLicense/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PermanentDrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm kontrollib kasutaja vanust
+            //programm teatab kasutajat, kas ta on piisavalt vana
+            //et juhiluba saada
+
+            Console.WriteLine("Sisesta oma sünniaasta.");
+
+            int born = Int32.Parse(Console.ReadLine());
+
+            if (born < 2003)
+            {
+                Console.WriteLine("Oled piisavalt vana, et saada juhiluba!");
+            }
+            else if (born > 2003)
+            {
+                Console.WriteLine("Oled liiga noor, et saada juhiluba!");
+            }
+            else
+            {
+                Console.WriteLine("Oled piisavalt vana, et saada juhiluba!");
+            }
+            Console.WriteLine("Head päeva!");
+
+        }
+    }
+}
---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub sisestada numbrit
+            //programm kontrollib numbrit
+            //kui number on positiivne, konsool kuvab "number on positiivne";
+            //kui number on negatiivne, konsool kuvab "number on negatiivne";
+            //kui sisestatud number on 0 siis konsool kuvab "null";
+            //NB! Int32.parse asemel kasutame  Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Palun sisesta number");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Number on negatiivne");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("Number on positiivne");
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
+
+
+        }
+    }
+}
